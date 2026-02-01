f3b76c4561e581c11780992e23721f02aaae2f25
fomean <ily.fomin@mail.ru>
2026-02-01 22:07:57 +0300

    Back to minimal test

--- .millennium/Dist/webkit.js
+++ .millennium/Dist/webkit.js
@@ -123,15 +123,7 @@ let PluginEntryPointMain = function() { var millennium_main = (function (exports
     'use strict';
 
     async function WebkitMain() {
-        console.log('test1');
-        setTimeout(() => {
-            console.log('test2');
-            const btn = document.createElement('button');
-            btn.textContent = 'X';
-            btn.style.cssText = 'position:fixed;top:50px;right:20px;z-index:999999';
-            document.body.appendChild(btn);
-            console.log('test3');
-        }, 3000);
+        console.log('test');
     }
 
     exports.default = WebkitMain;
--- webkit/index.tsx
+++ webkit/index.tsx
@@ -1,12 +1,3 @@
 export default async function WebkitMain() {
-  console.log('test1');
-
-  setTimeout(() => {
-    console.log('test2');
-    const btn = document.createElement('button');
-    btn.textContent = 'X';
-    btn.style.cssText = 'position:fixed;top:50px;right:20px;z-index:999999';
-    document.body.appendChild(btn);
-    console.log('test3');
-  }, 3000);
+  console.log('test');
 }
